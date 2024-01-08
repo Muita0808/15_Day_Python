1fcf27bdf0ff9832be6ba3009278ed329a4794a5
Muita0808 <magdalinemuita@gmail.com>
2024-01-09 00:39:43 +0300

    DAY 1

---
+++ main.py
@@ -0,0 +1,25 @@
+#program taking users name and age as input and prints a greeting message
+#QUIZ 2222222
+name=input("Enter your name: ")
+age =int(input("Enter your age: "))
+print("hello %s, are you %d years old" % (name, age))
+#
+##Quiz 4444444
+#FIND MA AND MINIMUM VALUE GIVEN  A LIST OF NUMBERS
+numbers = [2, 54, 7, 10, -3]
+max_value = max(numbers)
+min_value = min(numbers)
+print("Maximum value:", max_value)
+print("Minimum value:", min_value)
+#
+##QUIZ 66666
+#CALCULTING COMPOUND INTEREST
+#WE just take some random numbers p=5000, rate = 10%, time = 2yrs
+principal = 5000
+rate = 0.1
+time = 2 #in years
+amount = principal * (1 + rate) **time
+#INCOMPLETE ABOVE
+#
+#
+#QUIZ 8888
