8688e26ea9611d703121b91327b62d2bee3d627c
Muita0808 <magdalinemuita@gmail.com>
2024-01-10 06:57:58 +0300

    DAY 1

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 #program taking users name and age as input and prints a greeting message
 #QUIZ 2222222
 name=input("Enter your name: ")
-age =int(input("Enter your age: "))
-print("hello %s, are you %d years old" % (name, age))
+ages =int(input("Enter your age: "))
+print("hy %s, are you %d years old" % (name, ages))
 #
 ##Quiz 4444444
 #FIND MA AND MINIMUM VALUE GIVEN  A LIST OF NUMBERS
@@ -11,15 +11,17 @@ max_value = max(numbers)
 min_value = min(numbers)
 print("Maximum value:", max_value)
 print("Minimum value:", min_value)
-#
-##QUIZ 66666
-#CALCULTING COMPOUND INTEREST
-#WE just take some random numbers p=5000, rate = 10%, time = 2yrs
-#principal = 5000
-#rate = 0.1
-#time = 2 #in years
-#Amount = principal * (1 + rate) **time
 
-#
-#
-#QUIZ 8888
+####QUIZ 88888888
+#find sum of all positives given a list of intergers
+my_list = [2, 5, 8, -2, 4]
+result = sum([x for x in my_list if x > 0])
+print("the sum of positive numbers is:", result)
+##QUIZ 1010101
+x = 10
+y = 5
+temp = x  # Store the value of x in a temporary variable
+x = y     # Assign the value of y to x
+y = temp   # Assign the original value of x (stored in temp) to y
+print("After swapping: x =", x)
+print("After swapping: y =", y)
