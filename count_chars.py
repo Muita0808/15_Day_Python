ac01c994b5f1913234a4d042ab124dc80d1f3c24
NjengaC <victorcyrus01@gmail.com>
2024-02-05 20:52:48 +0300

    mike

---
+++ count_chars.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""
+program takes a sentence and counts words
+"""
+def count_char(sentence):
+    """
+    function takes a sentence and returns no of characters
+    """
+    if isinstance(str, sentence):
+        return len(sentence)
---
+++ test_countchars.py
@@ -0,0 +1,4 @@
+#!/usr/bin/python3
+from count_chars import count_char
+str = "This is a sentence"
+count_char(str)
